d84cadf3b254c07ab245a49470aa6edb71bfc363
Roman <romantimilsina@gmail.com>
2023-11-22 11:11:32 +0545

    temperature conversion app from celsius to kelvin and vice versa using swiftUI

--- TemperatureConversion/ContentView.swift
+++ TemperatureConversion/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var temperature: Double = 0.0
+    @State private var isCelsius = true
+    @State private var newTemperature: Double = 0.0
+    @State private var buttonText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Temperature"){
+                TextField("Enter temperature", value: $temperature, format: .number)
+                    .font(.title)
+            }
+            
+            Section(isCelsius ? "Celsius To Kelvin" : "Kelvin To Celsius"){
+//                HStack {
+                Button("\(!isCelsius ? "Celsius" : "Kelvin")") {
+                        if isCelsius {
+                            newTemperature = temperature + 273
+                            buttonText = "Kelvin"
+                            isCelsius = false
+                        } else {
+                            newTemperature = temperature - 273
+                            buttonText = "Celsius"
+                            isCelsius = true
+                        }
+                        
+                        //                        isCelsius = false
+                        
+                    }
+                    .padding()
+                    .font(.title)
+                    .background(.yellow)
+                    .frame(width: 150, height: 150)
+                    
+                
+            }
+            //            .padding()
+            Section("Result"){
+            
+                Text("\(round(newTemperature * 100) / 100) \(buttonText)")
+                    .font(.title)
+                
+            }
+            
         }
-        .padding()
     }
 }
 
